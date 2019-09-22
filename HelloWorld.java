713568d32cd6eeabb3332ac13ee7a48fd065ce45
EC2 Default User <ec2-user@ip-172-31-42-165.us-east-2.compute.internal>
2019-09-22 07:03:10 +0000

    First git commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class  HelloWorld
+{
+
+public static void main(String[] args)
+{
+  System.out.println("Hello Arvind");
+}
+}public class  HelloWorld
+{
+
+public static void main(String[] args)
+{
+  System.out.println("Hello Arvind");
+}
+}
